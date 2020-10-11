6c2794fa184c8be318f0d8f66e40b06d2ff5bb44
Hans Hsieh <hans@example.com>
2020-10-11 15:08:10 +0800

    Allow randomly pick images from a dir

--- src/main/java/org/comeonwallpaper/imgsource/factory/DirImgSourceFactory.java
+++ src/main/java/org/comeonwallpaper/imgsource/factory/DirImgSourceFactory.java
@@ -5,17 +5,20 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 import org.comeonwallpaper.conf.DataSource;
 import org.comeonwallpaper.imgsource.DirImgSource;
 import org.comeonwallpaper.imgsource.ImgSource;
-import org.comeonwallpaper.imgsource.RandomImgSource;
 
 import javax.validation.constraints.NotNull;
 import java.io.File;
-import java.util.ArrayList;
+import java.time.Duration;
 import java.util.List;
 
 public class DirImgSourceFactory extends ImgSourceFactory {
   private static class Settings {
     @NotNull
     public String path;
+    @NotNull
+    public DirImgSource.Mode mode;
+    @NotNull
+    public Long refreshIntervalMs;
   }
 
   public DirImgSourceFactory(@NonNull DataSource settings, @NonNull List<ImgSourceFactory> dependencies) {
@@ -26,6 +29,9 @@ public class DirImgSourceFactory extends ImgSourceFactory {
   public @NonNull ImgSource create() throws Exception {
     Validate.isTrue(dependencies.isEmpty(), getClass().getName() + " has no dependencies");
     Settings parsedSettings = settings.asType(Settings.class);
-    return new DirImgSource(new File(parsedSettings.path));
+    return new DirImgSource(
+        new File(parsedSettings.path),
+        parsedSettings.mode,
+        Duration.ofMillis(parsedSettings.refreshIntervalMs));
   }
 }
